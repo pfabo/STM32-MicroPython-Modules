df5a4198b8330b4f36b8260a5cfe036ac534a39c
Peter Fabo <38002305+pfabo@users.noreply.github.com>
2020-01-30 18:06:54 +0100

    Add files via upload

---
+++ blink/blink.py
@@ -0,0 +1,16 @@
+# deklaracie rozhrania
+
+def init()-> None:
+    """
+    Inicializacia portu a pinu s on-board LED
+    """
+
+
+def blink(n: int, delay: int) -> None:
+    """Blikanie LED
+    :param n: pocet bliknuti
+    :param b: delay
+    :return: None"""
+
+
+
